e6ae4669a75250687c00495cae1fc748c503f6ae
EmbarckleKeerthanaS <keerthana.s@embarckle.com>
2023-07-31 18:24:39 +0530

    Feat(frontend): Include Authprovider

--- frontend/src/Pages/index.tsx
+++ frontend/src/Pages/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Dashboard } from 'src/Components/Dashboard/Dashboard';
 import { Forgotpassword } from 'src/Components/forms/Forgotpassword';
 import { Resetpassword } from 'src/Components/forms/Resetpassword';
+import { Requireauth } from 'src/utils/RequireAuth';
 
 import { Signupform } from '../Components/Signupform';
 import { Loginpages } from './Loginpages';
@@ -13,6 +15,9 @@ export const Pages: React.FC = () => {
             <Route path='/signupform' element={<Signupform />} />
             <Route path='/forgotpassword' element={<Forgotpassword />} />
             <Route path='/resetpassword' element={<Resetpassword />} />
+            <Route element={<Requireauth />}>
+                <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
         </Routes>
     );
 };

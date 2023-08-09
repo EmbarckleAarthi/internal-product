fc9b6ed98fb7f992e3537d6060c67ebf73332e9d
EmbarckleKeerthanaS <keerthana.s@embarckle.com>
2023-08-09 12:46:21 +0530

    chore(client): Update config changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import './App.scss';
 
+import axios from 'axios';
 import React from 'react';
 
+import { config } from './Config';
 import { Pages } from './Pages';
+
+axios.defaults.baseURL = config.baseUrl;
 
 export const App: React.FC = () => {
     return (
